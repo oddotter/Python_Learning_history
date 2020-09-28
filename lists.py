3d86843169ef2633dcf93e84416f30f9527edeff
oddotter <48100118+oddotter@users.noreply.github.com>
2020-09-28 09:49:29 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,5 @@
+months = []
+months = [ "Jan", "Feb", "Mar", "Apr", "May", "June", "July","Aug", "Sept", "Oct", "Nov", "Dec"]
+months [5]
+len(months) #lenght of list
+#len(months[5]) # lenght of element 5 in list
