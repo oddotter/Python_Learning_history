d00af42c86743948abe8de8c032c1009d057f8d2
oddotter <48100118+oddotter@users.noreply.github.com>
2020-09-28 09:21:06 -0400

    Create creatringadef.py

---
+++ creatringadef.py
@@ -0,0 +1,8 @@
+def square_area():
+  lengthy = 5
+  widthx = 4
+  area_of_the_square = (lengthy) * (widthx)
+  print("The area of a square based on the data given is: ", area_of_the_square)
+
+
+square_area()
